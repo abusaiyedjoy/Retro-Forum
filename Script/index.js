24e80bd40a6662dc3d71f42d71e22a96c9df4e27
unknown <arafatshabbir8@gmail.com>
2024-03-03 21:03:11 +0600

    Complate responsive for tablet and dynamically setup

--- Script/index.js
+++ Script/index.js
@@ -4,20 +4,20 @@ const cartContainer = document.getElementById("cart-container");
 
 const letsDiscusePost = async () => {
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/retro-forum/posts"
+    'https://openapi.programming-hero.com/api/retro-forum/posts'
   );
   const data = await response.json();
   const allPosts = data.posts;
   allPosts.forEach((post) => {
-    // console.log(post)
+    console.log(post)
     const div = document.createElement("div");
     // div.classList = "flex gap-4 items-start mt-12";
     div.innerHTML = `<div class="w-full mb-12 lg:w-[80%] rounded-3xl shadow-lg p-10 border border-gray-300 bg-gray-100">
     <div class="relative flex gap-4 items-start">
-        <div class="w-[80px] h-[80px] rounded-2xl bg-white mt-3">
+        <div class="w-[70px] h-[70px] rounded-2xl bg-gray-100 mt-3">
             <div class="indicator w-full h-full flex justify-center items-center">
                 <span class="indicator-item badge bg-green-500"></span>
-                <div class="grid place-items-center">con</div>
+                <div class="grid place-items-center"><img class="w-full h-full rounded-xl" src="${post.image}" alt=""></div>
             </div>
         </div>
         <div class="w-full gap-4 bg-gray-100 flex flex-col p-3">
@@ -34,15 +34,15 @@ const letsDiscusePost = async () => {
                 <div class="flex gap-7">
                     <div class="flex items-center gap-3">
                         <i class="fa-regular fa-message"></i>
-                        <p class="text-xl">568</p>
+                        <p class="text-xl">${post.comment_count}</p>
                     </div>
                     <div class="flex items-center gap-3">
                         <i class="fa-regular fa-eye"></i>
-                        <p class="text-xl">1,576</p>
+                        <p class="text-xl">${post.view_count}</p>
                     </div>
                     <div class="flex items-center gap-3">
                         <i class="fa-regular fa-clock"></i>
-                        <p class="text-xl">5 min</p>
+                        <p class="text-xl">${post.posted_time} min</p>
                     </div>
                 </div>
                 <div>
@@ -102,17 +102,16 @@ const letestPost = async () => {
                 </div>
         `
         cartContainer.appendChild(div)
-      console.log(card)
     })
 };
 letestPost()
 
 
-const searchItem=()=>{
-    const inputValue = document.getElementById("inputSearch").value;
-    if(inputValue){
-        letsDiscusePost(inputValue)
-    }else{
-        alert('Please provide a valid input')
-    }
-}
+// const searchItem=()=>{
+//     const inputValue = document.getElementById("inputSearch").value;
+//     if(inputValue){
+//         letsDiscusePost(inputValue)
+//     }else{
+//         alert('Please provide a valid input')
+//     }
+// }
